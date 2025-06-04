05197c24eef3e51590b6df08743c8b4a57552626
Renan Collin <info@autoktone.net>
2025-06-04 14:38:12 +0200

    v1.3

--- back/main.py
+++ back/main.py
@@ -20,10 +20,4 @@ def predict():
 		
 @app.post("/predict1")
 def predict1():
-	try:
-		input_data = request.get_json()
-		r = requests.post(f"{PREDICT1_URL}/predict1", json=input_data)
-		# return jsonify(r.json())
-		return r.json()		
-	except Exception as e:
-        return {"error": str(e)}
+	return {"message": "Bienvenue sur /predict1"}
