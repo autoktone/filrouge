afc13edd58b4b57c64400530e1cd82ab773221bc
Renan Collin <info@autoktone.net>
2025-06-05 05:12:54 +0200

    v1.3

--- back-meteo/app.py
+++ back-meteo/app.py
@@ -5,22 +5,22 @@ app = Flask(__name__)
 
 # Input param : userlocation (town) au moment de l'appel à cet URI
 @app.route("/meteo")
-def get_meteo():
-	
-	# Mode bouchon
-	town = "Paris"
-	
-	# Google API Key can be stores in .env file
-	api_key = "7e0f2a976c6290e910b8ffcd4236982d"
-	
-	# Input param : userlocation au moment de l'appel
-    url = f"http://api.openweathermap.org/data/2.5/weather?q="+town+"&appid="+api_key+"&lang=fr&units=metric"
+def get_meteo(town: str = "Paris"):
+
+    # Google API Key can be stores in .env file
+    API_KEY = "7e0f2a976c6290e910b8ffcd4236982d"
+
+    # Input param : userlocation au moment de l'appel
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={town}&appid={API_KEY}&lang=fr&units=metric"
     response = requests.get(url)
-    data = response.json()
+    
+    if response.status_code != 200:
+        return {"error": f"Erreur API météo: {response.status_code}"}
 
     # Exemple : 'Rain', 'Clear', 'Clouds'...
+	data = response.json()
     condition = data['weather'][0]['main'].lower()
-    return jsonify({"weather": condition})
+    return jsonify({"weather": condition})	
 
 # Execution via Flask
 if __name__ == "__main__":
