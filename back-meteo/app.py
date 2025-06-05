0b474ca3a85c780e24bad3f9146378613c9d56f9
Renan Collin <info@autoktone.net>
2025-06-05 04:25:01 +0200

    v1.3

--- back-meteo/app.py
+++ back-meteo/app.py
@@ -1,12 +1,22 @@
 from flask import Flask, jsonify
+import requests
 
 app = Flask(__name__)
 
+# Input param : userlocation (town) au moment de l'appel à cet URI
 @app.route("/meteo")
-def get_meteo():
+def get_meteo(town="Paris"):
+	# Google API Key can be stores in .env file
+	api_key = "7e0f2a976c6290e910b8ffcd4236982d"
 	
 	# Input param : userlocation au moment de l'appel
-    return jsonify({"weather": "Rainy"})
+    url = f"http://api.openweathermap.org/data/2.5/weather?q="+town+"&appid="+api_key+"&lang=fr&units=metric"
+    response = requests.get(url)
+    data = response.json()
+
+    # Exemple : 'Rain', 'Clear', 'Clouds'...
+    condition = data['weather'][0]['main'].lower()
+    return jsonify({"weather": condition})
 
 # Execution via Flask
 if __name__ == "__main__":
